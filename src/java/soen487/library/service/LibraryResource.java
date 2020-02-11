1dc60e61f5e9e777b923fe31770f832854cc410e
Xavier Vani-Charron <xavier.vani@gmail.com>
2020-02-11 11:34:35 -0500

    Added more methods

--- src/java/soen487/library/service/LibraryResource.java
+++ src/java/soen487/library/service/LibraryResource.java
@@ -5,19 +5,26 @@
  */
 package soen487.library.service;
 
+import java.util.ArrayList;
+import soen487.library.system.Library;
+import soen487.library.core.Book;
+
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 /**
  *
- * @author xavie
+ * @author Xavier Vani-Charron
  */
 @Path("books")
 public class LibraryResource {
@@ -25,19 +32,67 @@ public class LibraryResource {
     @GET
     @Path("/list")
     @Produces(MediaType.TEXT_PLAIN)
-    public String listBooks(){
-        //call the library system function here for data dump
-        return "List of all books";
+    public String listAllBooks(){
+        
+        String output = "";
+        
+        Library lib = Library.getInstance();
+        
+        ArrayList<Book> allBooks = lib.read(null);
+        
+        if(allBooks.isEmpty()){
+            output = "No books in the Library System.";
+        }else{
+            for (Book book : allBooks){
+                output += book.toString();
+            }
+        }
+        
+        return output;
     }
     
     @GET
     @Path("/{id}")
     @Produces(MediaType.TEXT_PLAIN)
     public String getBook(@PathParam("id") String id){
-        return "This will return the book by id " + id;
+        
+        String result = null;
+        
+        Library lib = Library.getInstance();
+        
+        Long longID = Long.parseLong(id);
+        
+        ArrayList<Book> singleBook = lib.read(longID);
+        
+        if(singleBook.isEmpty()){
+            result = "No book with the id: "+id;
+        }else{
+            result = singleBook.get(0).toString();
+        }
+        
+        return result;
     }
     
-//    @POST
-//    @Consumes
+    @POST
+    @Path("/create")
+//    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void post(@FormParam("title") String title, @FormParam("description") String description, @FormParam("isbn") String isbn, @FormParam("author") String author, @FormParam("publisher") String publisher){
+        
+        Library lib = Library.getInstance();
+        
+        lib.create(description, title, isbn, author, publisher);
+    }
+    
+    @PUT
+    @Path("/edit/{id}")
+    public void editBook(){
+        
+    }
+    
+    @DELETE
+    @Path("delete/{id")
+    public void deleteBook(){
+        
+    }
     
 }
